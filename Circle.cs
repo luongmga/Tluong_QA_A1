1084971f12d9916b2ee0edfbcbf1e9c326cdb05c
luongmga <57503471+luongmga@users.noreply.github.com>
2020-02-11 11:13:53 -0500

    app done
    app done

---
+++ Circle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tluong_QA_A1
+{
+    public class Circle
+    {
+        private int _radius;
+        public int Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                // ensure non-negative radius value 
+                if (value >= 1)
+                    _radius = value;
+            }
+        }
+
+        //Default constructor
+        public Circle()
+        {
+            this.Radius = 1;
+        }
+        //Custom constructor
+        public Circle (int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            Radius = _radius;
+            return Radius;
+        }
+
+        public void SetRadius(int Rad)
+        {
+            if (Rad >= 0)
+            _radius = Rad;
+        }
+
+        public double GetCircumference()
+        {
+            int r = GetRadius();
+            double C = 2 * Math.PI * r;
+            return C;
+        }
+
+        public double GetArea()
+        {
+            int r = GetRadius();
+            double A = Math.PI * r * r;
+            return A;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,49 @@ namespace Tluong_QA_A1
     {
         static void Main(string[] args)
         {
+            Circle circle = new Circle();
+            string userInput;
+            do
+            {
+                ShowMenu();
+                userInput = Console.ReadLine();
+            
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Radius = " + circle.GetRadius());
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter your radius:");
+                        userInput = Console.ReadLine();
+                        circle.SetRadius(Convert.ToInt32(userInput));
+                        Console.WriteLine("Radius changed to " + circle.GetRadius());
+                        break;
+
+                    case "3":
+                        Console.WriteLine("The Circumferenece of the circle is : " + circle.GetCircumference());
+                        break;
+
+                    case "4":
+                        Console.WriteLine("The area of the circle is : " + circle.GetArea());
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Application closing!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong selection, please try again!");
+                        break;
+                }
+            }
+            while (userInput != "5");
+            Console.ReadKey();
+        }
+        static void ShowMenu()
+        {
+            Console.WriteLine( "Please select the following option: \n 1. Get Circle \n 2. Change Circle Radius \n 3. Get Circle Circumference \n 4. Get Circle \n 5. Exit");
         }
     }
 }
